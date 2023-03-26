75a494c771edce54933583fd0b1992a4a823028a
Alexey Kovalov <kovalov.alexey@gmail.com>
2023-03-26 15:48:13 +0000

    js-hw-7 1st task done

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const makeGalleryCard = ({preview, original, description}) => 
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+;
+
+const handleEscapeKey = () => {
+
+    document.removeEventListener('keydown', handleEscapeKey);
+}
+
+const markup = galleryItems.map((element) => makeGalleryCard(element)).join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", markup);
+
+galleryEl.addEventListener('click', event => {
+    event.preventDefault();
+  
+    if (event.target.nodeName !== 'IMG') {
+      return;
+    }
+  
+    const largeImageUrl = event.target.dataset.source;
+  
+    const instance = basicLightbox.create(`
+    <img src="${largeImageUrl}" width="800" height="600">`)
+    instance.show()
+
+    document.addEventListener('keydown', event => {
+        if (event.code === 'Escape') { instance.close()}
+  });
+});
